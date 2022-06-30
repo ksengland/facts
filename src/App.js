bd2396592f3f96dbb3237338a084e065f47c5f51
Karleen England <karleenengland@runbox.com>
2022-06-29 21:02:13 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Movie from "./Movie";
 
-function App() {
+const App = () => {
+  const url =
+    "https://api.themoviedb.org/3/movie/popular?api_key=bc243bbf57d0b71550a65828cdd9a0b1&language=en-US&page=1";
+
+  const [popular, setPopular] = useState([]);
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+  const fetchPopular = async () => {
+    const data = await fetch(url);
+    const movies = await data.json();
+    console.log(movies);
+    setPopular(movies.results);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Today's Popular Movies</h1>
+      <div className="popular-movies">
+        {popular.map((movie) => {
+          return <Movie key={movie.id} movie={movie} />;
+        })}
+      </div>
     </div>
   );
-}
-
+};
 export default App;
